1e7c1961b4aa5d49ebe652d3b989c2006bc0de5b
wuresearchgroup <164107448+wuresearchgroup@users.noreply.github.com>
2025-05-24 00:34:00 +0000

    Deploying to gh-pages from @ wuresearchgroup/wuresearchgroup.github.io@fbe1f23b356b2a28e45cce854e978823fbea31b4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-research",
+  },{id: "nav-gallery",
+          title: "Gallery",
+          description: "Some beautiful moments",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/gallery/";
+          },
+        },{id: "nav-research",
           title: "Research",
           description: "Everything Should Be Made as Simple as Possible, But Not Simpler --- Albert Einstein",
           section: "Navigation",
